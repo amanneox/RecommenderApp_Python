af43a2b61daba9ad206024ceba4e43c40670f590
Aman Adhikari <amanadhikari2@gmail.com>
2018-06-05 04:24:44 +0530

    added url

--- RecommenderPython/settings.py
+++ RecommenderPython/settings.py
@@ -12,7 +12,10 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 
 import os
 from mongoengine import *
-connect( db='cluster0-q36l8', username='amanneox', password='amanneox', host='mongodb+srv://amanneox:amanneox@cluster0-q36l8.mongodb.net/test?retryWrites=true')
+import pymongo
+client = pymongo.MongoClient("mongodb+srv://amanneox:amanneox@cluster0-q36l8.mongodb.net/test?retryWrites=true")
+
+#connect( db='cluster0-q36l8', username='amanneox', password='amanneox', host='mongodb+srv://amanneox:amanneox@cluster0-q36l8.mongodb.net/test?retryWrites=true')
 #connect('mongodb+srv://amanneox:amanneox@cluster0-q36l8.mongodb.net/test?retryWrites=true')
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
