5055207a512317d57cc9ca095e22a9ca11f2bdd7
Aman Adhikari <amanadhikari2@gmail.com>
2018-06-05 05:52:36 +0530

    setting

--- RecommenderPython/settings.py
+++ RecommenderPython/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 
 import os
 from mongoengine import *
-
 connect(host='mongodb+srv://amanneox:amanneox@cluster0-q36l8.mongodb.net/test?retryWrites=true'
 )
 #connect('mongodb+srv://amanneox:amanneox@cluster0-q36l8.mongodb.net/test?retryWrites=true')
