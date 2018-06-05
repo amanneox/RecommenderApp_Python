030ac21cb0424a8c22a8a96fc88d4154698c8a88
Aman Adhikari <amanadhikari2@gmail.com>
2018-06-05 06:44:51 +0530

    Added Location

--- api/models.py
+++ api/models.py
@@ -4,12 +4,30 @@ from django.db import models
 from mongoengine import *
 
 
-
 class Service(Document):
     name=ListField(StringField(max_length=20,required=True))
     category=ListField(StringField(required=True))
     subcategory=ListField(StringField(required=True))
     status=ListField(BooleanField(required=True))
+
+
+class LatLng(EmbeddedDocument):
+    lat=StringField()
+    lng=StringField()
+
+class Address(EmbeddedDocument):
+    street=StringField()
+    pincode=StringField()
+    city=StringField()
+    state=StringField()
+    country=StringField()
+
+
+class Item(Document):
+    name=ListField(StringField(required=True))
+    category=ListField(ReferenceField(Service))
+    location=ListField(EmbeddedDocumentListField(LatLng))
+    address=ListField(EmbeddedDocumentListField(Address))
 
 
 class User(Document):
@@ -30,7 +48,7 @@ class Post(Document):
     title = ListField(StringField(max_length=120, required=True))
     author = ListField(ReferenceField(User, reverse_delete_rule=CASCADE))
     tags = ListField(StringField(max_length=30))
-    comments = ListField(EmbeddedDocumentField(Comment))
+    comments = ListField(EmbeddedDocumentListField(Comment))
     rating=ListField(FloatField())
     sid=ListField(ReferenceField(Service,required=True))
     meta = {'allow_inheritance': True}
